482f3ab8064eb221cbb51d50aeb449a148452405
dolphin9 <dolphin9chy@gmail.com>
2017-05-31 16:37:33 +0800

    尝试侧边栏

--- usr/register.php
+++ usr/register.php
@@ -1,51 +1,50 @@
-<?php
-require_once("connect.php");
-//error_reporting(0);
-$usrname = $pwd = $idnum = "";
-$name = $email = $phone = "";
-$sex = $accountname = $fmaddr = "";
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    require_once("connect.php");
+    //error_reporting(0);
+    $usrname = $pwd = $idnum = "";
+    $name = $email = $phone = "";
+    $sex = $accountname ="";
+    $state = "";
 
-$usrname = htmlspecialchars($_POST['usrname']);
-$pwd = MD5($_POST['pwd']);
-$name = htmlspecialchars($_POST['name']);
-$email = htmlspecialchars($_POST['email']);
-$phone = htmlspecialchars($_POST['phone']);
-$idnum = htmlspecialchars($_POST['idnum']);
-$sex = htmlspecialchars($_POST['sex']);
-$accountname = htmlspecialchars($_POST['accountname']);
-$fmaddr = htmlspecialchars($_POST['fmaddr']);
-
-//$usrname = "Anne";
-//$pwd = "123456";
-//$name = "Anne Willians";
-//$email = "Anne@Willians.com";
-echo "hew";
-echo "<br>"."$usrname";
-echo "<br>"."$";
-echo "<br>"."$sex";
-echo "<br>"."$fmaddr";
-echo "<br>"."$name";
-echo "<br>";
+    $usrname = htmlspecialchars($_POST['usrname']);
+    $pwd = MD5($_POST['pwd']);
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $idnum = htmlspecialchars($_POST['idnum']);
+    $sex = htmlspecialchars($_POST['sex']);
+    $accountname = htmlspecialchars($_POST['accountname']);
 
 
-//Part2 insert 连接数据库并向数据库中插入值， 这里为注册操作
-//      此处使用的user为writer，database为stock，table为nomalusr
-//      默认输入的信息都是合法的。
-function usrregister($usrname,$pwd,$name,$email,$phone,$sex,$idnum,$accountname){
-  $con = connectMySQL('writer');
-  mysql_select_db("stock" , $con);
-  $sql = "INSERT INTO normalusr (usrname,pwd,email,phone,name,sex,idnum,  accountname)
-  VALUES
-  ( '$usrname' , '$pwd' ,  '$email' , '$phone' ,'$name' , '$sex', '$idnum',    '$accountname'  )
-  ";
-  if(!mysql_query($sql,$con))
-  {
-    die('Error: ' . mysql_error());
-  }
-  echo "register ok!";
-  mysql_close($con);
-}
 
-usrregister($usrname,$pwd,$name,$email,$phone,$sex,$idnum,$accountname);
+    //Part2 insert 连接数据库并向数据库中插入值， 这里为注册操作
+    //      此处使用的user为writer，database为stock，table为nomalusr
+    //      默认输入的信息都是合法的。
+    function usrregister($usrname,$pwd,$name,$email,$phone,$sex,$idnum,$accountname){
+      $con = connectMySQL('writer');
+      mysql_select_db("stock" , $con);
+      $sql = "INSERT INTO normalusr (usrname,pwd,email,phone,name,sex,idnum,  accountname)
+      VALUES
+      ( '$usrname' , '$pwd' ,  '$email' , '$phone' ,'$name' , '$sex', '$idnum',    '$accountname'  )
+      ";
+      if(!mysql_query($sql,$con))
+      {
+        die('Error: ' . mysql_error());
+      }
+      echo "register ok!";
+      mysql_close($con);
+    }
 
-?>
+    usrregister($usrname,$pwd,$name,$email,$phone,$sex,$idnum,$accountname);
+
+    ?>
+    <script language='javascript'>document.location = '../index.html''</script>
+  </body>
+</html>
